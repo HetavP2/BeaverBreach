011eae1fcfe28a787a647b7caf4e8dc52e94c69a
HetavP2 <hetav.j.patel@gmail.com>
2025-02-22 00:25:27 -0500

    tailwind setup

--- run.py
+++ run.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template
-
 import firebase_admin
 from firebase_admin import credentials as cred
 from firebase_admin import db
 
-app=Flask(__name__)
+app= Flask(__name__)
 
 @app.route('/')
 def home():
