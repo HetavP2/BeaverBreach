42f6eac96eadd2b3658fd73d02eb8ddbdf264e68
HetavP2 <hetav.j.patel@gmail.com>
2025-02-22 10:47:40 -0500

    trying to setup firebase

--- run.py
+++ run.py
@@ -1,15 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 import firebase_admin
-from firebase_admin import credentials as cred
-from firebase_admin import db
+from firebase_admin import credentials, firestore 
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
-import pandas,os
-from werkzeug.utils import secure_filename
+import pandas, dotenv, os
+
+dotenv.load_dotenv(".env")
 
 app= Flask(__name__)
-app.config['SECRET_KEY']="123443525"
-app.config['UPLOAD_FOLDER']="allfiles"
+app.config['SECRET_KEY']=os.getenv("SECRET_KEY")
+firebase_admin.initialize_app(credentials.Certificate("serviceAccountKey.json"), {
+    'databaseURL': ''
+})
+db = firestore.client()
+
+# Add data to Firestore
+doc_ref = db.collection("proposedchange").document("user1")
+# doc_ref.set({"name": "Alice", "email": "alice@example.com"})
+print(db)
+
+# Retrieve data
+# user_doc = db.collection("users").document("user1").get()
+# if user_doc.exists:
+#     print(user_doc.to_dict())
 
 class uploadFile(FlaskForm):
     file=FileField("File")
@@ -25,10 +38,7 @@ def dashboard():
     if form.validate_on_submit():
         file = form.file.data
         data = pandas.read_csv(file)
-
-        
         columns_list = data.columns
-
         for index, row in data.iterrows():
             print(index, row)
         
